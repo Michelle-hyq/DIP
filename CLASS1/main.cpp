45aa2d9dafb2b45c7d171db621d9cf2bbdf5615b
芸倩 何 <1453715165@qq.com>
2020-10-05 23:06:14 +0800

    直方图计算机显示部分完成

--- CLASS1/main.cpp
+++ CLASS1/main.cpp
@@ -99,14 +99,64 @@ void test2_3()//画圆、直线、矩形
 	imshow("circle", srcMat);
 	waitKey(0);
 }
-void test2_4()//直方图计算
+void test2_4()//直方图计算与显示
 {
-	float histgram[256];
-	cv::Mat srcMat = imread("D://image/timg1.jpg");
-
+	int width, height, channels,p_sum;
+	int i, j;
+	int dirta=2;
+	int x0 = 10, y0 =420;
+	int gray_value[256] = { 0 }, gray_temp,gray_beishu;
+	float histgram[256] = { 0 },gray_max=0;
+	cv::Mat srcMat = imread("D://image/timg1.jpg",0);
+	height = srcMat.rows;
+	width = srcMat.cols;
+	printf("width: %d\r\n", width);
+	printf("height:%d\r\n", height);
+	//上面两句等价于下面两句
+	//cout << "height = " << height << endl
+	//	<< "width = " << width << endl;
+	channels = srcMat.channels();
+	p_sum = width * height;//像素点数
+	//计算256阶灰度的像素点个数
+	for ( i = 0; i < height; i++){
+		uchar* data = srcMat.ptr<uchar>(i);//第i行 纵坐标
+		for ( j = 0; j < width; j++){
+			gray_temp = data[j];
+			gray_value[gray_temp]++;
+			//printf("gray value = %d\n", data[j]);
+		}
+	}
+	//计算256阶灰度的像素点概率
+	for (i = 0; i < 256; i++){
+		histgram[i] = (float)gray_value[i] / p_sum;
+		if (histgram[i] > gray_max)//找最大值
+			gray_max = histgram[i];
+		//printf("gray value = %f\n", histgram[i]);
+	}
+	gray_beishu = 400 / gray_max;//计算倍数 归一化到400以便显示
+	//画直方图
+	cv::Rect rect;
+	cv::Mat zhifangtu = Mat::zeros(450, 550, CV_8UC3);//画黑色背景
+	for (i = 0; i < 256; i++){
+		gray_temp=(int)(histgram[i]*gray_beishu);
+		printf("gray value = %d\n", gray_temp);
+		rect.x = x0 + i * dirta;
+		rect.y = y0 - gray_temp;//从上往下画
+		rect.width = dirta;
+		rect.height = gray_temp;
+		rectangle(zhifangtu, rect, CV_RGB(255, 0, 0), 1, 8, 0);//红色
+	}
+	imshow("zhifangtu", zhifangtu);
+	waitKey(0);/**/
 }
 int main()
 {
-	test2_3();
+	//test2_1();	//第一题
+	//test2_2();	//第二题
+	//test2_3_1();	//画圆
+	//test2_3_2();	//直线
+	//test2_3_3();	//矩形
+	//test2_3();	 //画圆、直线、矩形
+	test2_4();
 	return 0;
 }/**/
