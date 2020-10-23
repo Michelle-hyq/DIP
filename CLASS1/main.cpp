4acb23b5164a24bc8c67f48cce298dda1e9a08cb
芸倩 何 <1453715165@qq.com>
2020-10-23 15:58:45 +0800

    完成

--- CLASS1/main.cpp
+++ CLASS1/main.cpp
@@ -30,7 +30,7 @@ void test3_1()
 		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
 
 		rFrame.copyTo(detectMat);
-		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV),detectMat);
+		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
 		imshow("while: in the range", detectMat);
 		imshow("frame", rFrame);
 		waitKey(3);
@@ -39,7 +39,7 @@ void test3_1()
 //-------练习2 二值化的两种方法--------------------------------------------------------//
 void test3_2()//二值化的两种方法
 {
-	cv::Mat srcMat = imread("D://image/timg1.jpg",0);
+	cv::Mat srcMat = imread("D://image/timg1.jpg", 0);
 	cv::Mat dstMat;
 	//threshold(srcMat, dstMat, 100, 255, THRESH_BINARY);
 	//-----------------100阈值--255预设满足条件的最大值--使用的二值化算法
@@ -64,7 +64,7 @@ void threshold_Mat(int th, void* data)
 int test3_3()
 {
 	//Mat srcMat = imread("D://image/timg1.jpg");
-	Mat gryMat = imread("D://image/timg1.jpg",0);
+	Mat gryMat = imread("D://image/timg1.jpg", 0);
 	int lowTH = 30;
 	int maxTH = 255;
 	if (!gryMat.data)
