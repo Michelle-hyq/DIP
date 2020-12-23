cddab39f10ece1f7664ef9cc3cec7d6964a105b4
芸倩 何 <1453715165@qq.com>
2020-12-23 10:30:33 +0800

    练习1完成

--- CLASS1/main.cpp
+++ CLASS1/main.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-// 课堂十二
+// 课堂十三 
+//模板匹配功能调用练习
 //--------------------------------------------------------------------------------------------//
-//练习1 背景差分
-cv::Mat frame, bgMat, subMat, bny_subMat;
+//练习1 
+cv::Mat frame, tempMat, refMat, resultMat;
 VideoCapture cap;
 cv::Mat frame_out;
-void test12_1()
+void test13_1()
 {
 	cap.open(0);
 	while (!cap.isOpened()); {
@@ -18,222 +19,183 @@ void test12_1()
 		std::cout << "不能打开视频文件" << std::endl;
 	}
 	int cnt = 0;
-
+	int match_method = 0;
+	double minVal, maxVal;
+	Point matchLoc;
+	Point minLoc, maxLoc;
 	while(1){
 		cap >> frame;
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
 		if (cnt == 0) {
-			frame.copyTo(bgMat);
+			Rect2d r;
+			r = selectROI(frame, true);
+			tempMat = frame(r);
+			tempMat.copyTo(refMat);
+			//namedWindow("tempMat", WINDOW_AUTOSIZE);
+			//imshow("tempMat", tempMat);
+			//waitKey(0);
+			destroyAllWindows();
+			cnt = 1;
 		}
 		else {
-			//第二帧开始背景差分
-			//相减
-			absdiff(frame, bgMat, subMat);
-			//absdiff(bgMat, frame, subMat);
-			//差分结果二值化
-			threshold(subMat, bny_subMat, 50, 255, CV_THRESH_BINARY);
-			imshow("b_subMat", bny_subMat);
-			imshow("subMat", subMat);
-			waitKey(30);
+			//第二帧开始
+			
+			matchTemplate(frame, refMat, resultMat, match_method);//寻找匹配区域
+			//imshow("resultMat", resultMat);
+			normalize(resultMat, resultMat, 0, 1, NORM_MINMAX);//归一化
+			//imshow("resultMat2", resultMat);
+			minMaxLoc(resultMat, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+			rectangle(frame, minLoc, Point2i(minLoc.x + tempMat.cols, minLoc.y + tempMat.rows),Scalar(0,0,0),2,8,0);
+			namedWindow("frame", WINDOW_AUTOSIZE);
+			imshow("frame", frame);
+			//imshow("subMat", subMat);
+			waitKey(30);	
 		}
-		cnt = 1;
 	}
 }
-//--------------------------------------------------------------------------------------------//
-//高斯建模及背景差分
-void test12_2()
+cv::Mat binMat, gryMat, minRectMat, canny_img, dstMat;
+int nX, nY;
+int bins = 810;
+float ref_hist[810];
+float pl_hist[810];
+float bg_hist[810];/**/
+void calcHOG(cv::Mat src, float* tempHist, int nAngle, int cellSize)
 {
-	cap.open(0);
-	while (!cap.isOpened()); {
-		cap.open(0);
-		std::cout << "不能打开视频文件" << std::endl;
+	int i, j;
+	Rect roi;
+	roi.width = cellSize;
+	roi.height = cellSize;
+	//计算所有像素的梯度与角度方向，统计直方图
+	Mat gx, gy;
+	Sobel(src, gx, CV_32F, 1, 0, 1);
+	Sobel(src, gy, CV_32F, 0, 1, 1);
+
+	cv::Mat roiMat;
+	cv::Mat roiMag;
+	cv::Mat roiAgl;
+
+	//默认是弧度radians，可选择角度degrees
+	Mat mag, angle;//每个像素点都有幅值和方向
+	cartToPolar(gx, gy, mag, angle, true);
+	int cont = 0;
+	for (int i = 0; i < nY; i++) {
+		for (int j = 0; j < nX; j++) {
+			cont++;
+			roi.x = j * cellSize;
+			roi.y = i * cellSize;
+			//赋值图像 ROI感兴趣的区域
+			roiMat = src(roi);
+			roiMag = mag(roi);
+			roiAgl = angle(roi);
+			//当前cell第一个像素在数组中的位置
+			int head = (i * nX + j) * nAngle;
+			//ref_hist += head;
+
+			//计算角度直方图
+			//cell存储空间信息 不同空间有一个9维直方图
+			//第0个-8个 代表梯度角度0 20 40...160 °
+			//值表示梯度幅度
+			int cnt = 0;
+			for (int n = 0; n < roiMat.rows; n++) {
+				for (int m = 0; m < roiMat.cols; m++) {
+					cnt++;
+					float temp_mag = (roiMag.at<float>(m, n));//像素点的幅度值mag
+					int temp_ang = (int)(roiAgl.at<float>(m, n));
+					float show_ang = roiAgl.at<float>(m, n);
+					//cout << "\temp_ang: " << show_ang << endl;
+
+					if (temp_ang % 40 == 0)
+						tempHist[head + temp_ang / 40] = temp_mag;
+					//*(ref_hist + temp_ang / 40) = temp_mag;
+					else if (temp_ang / 40 < 9) {
+						tempHist[head + temp_ang / 40] = (float)(40 - (temp_ang % 40)) / 40.0 * temp_mag;
+						tempHist[head + ((temp_ang / 40) == 8 ? (temp_ang / 40 + 1) : 0)] = (float)(temp_ang % 40) / 40 * temp_mag;/**/
+						/**(tempHist + temp_ang / 40) = (float)(40-(temp_ang % 40)) / 40.0 * temp_mag;
+						*(tempHist + ((temp_ang/40)==8? (temp_ang / 40+1):0)) = (float)(temp_ang % 40) / 40.0 * temp_mag;/**/
+					}
+
+				}
+			}
+		}
 	}
+}
+void test13_2()
+{
 	int cnt = 0;
+	int match_method = 0;
+	double minVal, maxVal;
+	Point matchLoc;
+	Point minLoc, maxLoc;
 
-	while (1) {
-		cap >> frame;
-		cvtColor(frame, frame, COLOR_BGR2GRAY);
+	frame = imread("D://image/timg7.jpg");
+	cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+	Mat srcMat, plMat, bgMat;
+	srcMat = imread("D://image/timg7.jpg");
+	cvtColor(srcMat, gryMat, COLOR_BGR2GRAY);//灰度图
+	int wid = 0;
+	int hei = 0;
+	wid = srcMat.cols;
+	hei = srcMat.rows;
+	int area = wid * hei;
+
+	Rect2d r;
+	r = selectROI(frame, true);
+	tempMat = frame(r);
+	tempMat.copyTo(refMat);
+	//srcMat.copyTo(refMat);
+	//图像划分为16*16的cell
+	int blockSize = 16;
+	nX = refMat.cols / blockSize;//横方向上有几个cell
+	nY = refMat.rows / blockSize;//竖方向上有几个cell
+	int nAangle = 9;//180°n等分 0 20 40 160 九等分
+	int bins = nX * nY * nAangle;//每个cell需要9bit的直方统计图
+	cout << "bins: " << bins << endl;
+	while (1) {	
+		
+		//计算三张输入图片的HOG
+		calcHOG(refMat, ref_hist, nAangle, blockSize);//ref_hist直方图
+		for (int i = 0; i < 10; i++) printf("%f ", ref_hist[i]);
+		printf("\r\n ");
+		//计算直方图距离，相似度
+		//float dis0 = normL2Sqr(ref_hist, ref_hist, bins);
+		float dis1 = normL2Sqr(ref_hist, pl_hist, bins);
+		cout << "dis1: " << dis1 << endl;
+		namedWindow("标记原图", WINDOW_AUTOSIZE);
+		imshow("标记原图", srcMat);
+		waitKey(0);
+
 		if (cnt == 0) {
-			frame.copyTo(bgMat);
+			Rect2d r;
+			r = selectROI(frame, true);
+			tempMat = frame(r);
+			tempMat.copyTo(refMat);
+			//namedWindow("tempMat", WINDOW_AUTOSIZE);
+			//imshow("tempMat", tempMat);
+			//waitKey(0);
+			destroyAllWindows();
+			cnt = 1;
 		}
 		else {
-			//第二帧开始背景差分
-			//相减
-			absdiff(frame, bgMat, subMat);
-			//absdiff(bgMat, frame, subMat);
-			//差分结果二值化
-			threshold(subMat, bny_subMat, 45, 255, CV_THRESH_BINARY);
-			imshow("b_subMat", bny_subMat);
-			imshow("subMat", subMat);
+			//第二帧开始
+
+			matchTemplate(frame, refMat, resultMat, match_method);//寻找匹配区域
+			//imshow("resultMat", resultMat);
+			normalize(resultMat, resultMat, 0, 1, NORM_MINMAX);//归一化
+			//imshow("resultMat2", resultMat);
+			minMaxLoc(resultMat, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+			rectangle(frame, minLoc, Point2i(minLoc.x + tempMat.cols, minLoc.y + tempMat.rows), Scalar(0, 0, 0), 2, 8, 0);
+			namedWindow("frame", WINDOW_AUTOSIZE);
+			imshow("frame", frame);
+			//imshow("subMat", subMat);
 			waitKey(30);
 		}
-		cnt = 1;
 	}
+	
 }
 
-int median_size = 5;
-void med_fil_sizeChange(int, void*);
-void med_proc() {
-	bool rSucess = cap.read(frame);
-	if (!rSucess) {
-		std::cout << "不能从视频文件读取帧" << std::endl;
-	}
-	namedWindow("中值原图", WINDOW_NORMAL);
-	imshow("中值原图", frame);
-	medianBlur(frame, frame_out, median_size * 2 + 1);//一直出错，不能为偶数
-	namedWindow("中值滤波", WINDOW_NORMAL);
-	imshow("中值滤波", frame_out);
-	waitKey(30);
-}
-void med_fil_sizeChange(int, void*)
-{
-	med_proc();
-}
-void test5_1()
-{
-	cap.open(0);
-	while (!cap.isOpened()); {
-		cap.open(0);
-		std::cout << "不能打开视频文件" << std::endl;
-	}
-
-	med_proc();
-	createTrackbar("中值尺寸", "中值滤波", &median_size, 10, med_fil_sizeChange);
-	while (1)
-	{
-		med_proc();
-		createTrackbar("中值尺寸", "中值滤波", &median_size, 10, med_fil_sizeChange);
-		//cvSetTrackbarPos("滤波尺寸", "中值滤波", median_size);	
-	}
-	/*
-	createTrackbar(
-		"threshold",//轨迹条的名字
-		window_name,//轨迹条依附的窗口名字
-		&lowTH,		//指向整型的指针，表示滑块的位置，在创建时，滑块的初始位置就是该变量的初始值
-		maxTH,		//滑块可以到达的最大位置的值
-		threshold_Mat,//指向回调函数的指针
-		&gryMat		//用户传给回调函数的数据
-	);/**/
-}
-//--------------------------------------------------------------------------------------------//
-//练习2 均值滤波 读取摄像头图像，并对摄像头图像进行均值滤波
-int avr_size = 3;
-void avr_fil_sizeChange(int, void*);
-void avr_proc() {
-	bool rSucess = cap.read(frame);
-	if (!rSucess) {
-		std::cout << "不能从视频文件读取帧" << std::endl;
-	}
-	namedWindow("均值原图", WINDOW_NORMAL);
-	imshow("均值原图", frame);
-	if (avr_size == 0) avr_size = 1;//尺寸不能为0
-	blur(frame, frame_out, Size(avr_size, avr_size));
-	namedWindow("均值滤波", WINDOW_NORMAL);
-	imshow("均值滤波", frame_out);
-	waitKey(30);
-}
-void avr_fil_sizeChange(int, void*)
-{
-	avr_proc();
-}
-void test5_2()
-{
-	cap.open(0);
-	while (!cap.isOpened()); {
-		cap.open(0);
-		std::cout << "不能打开视频文件" << std::endl;
-	}
-	avr_proc();
-	createTrackbar("均值尺寸", "均值滤波", &avr_size, 10, avr_fil_sizeChange);
-	while (1)
-	{
-		avr_proc();
-		createTrackbar("均值尺寸", "均值滤波", &avr_size, 10, avr_fil_sizeChange);
-	}
-}
-//--------------------------------------------------------------------------------------------//
-//练习3 均值滤波-高斯滤波 读取摄像头图像，并对摄像头图像进行高斯滤波
-int Gauss_size = 5;
-void Gauss_fil_sizeChange(int, void*);
-void Gauss_proc() {
-	bool rSucess = cap.read(frame);
-	if (!rSucess) {
-		std::cout << "不能从视频文件读取帧" << std::endl;
-	}
-	namedWindow("高斯原图", WINDOW_NORMAL);
-	imshow("高斯原图", frame);
-	if (Gauss_size == 0) Gauss_size = 1;//尺寸不能为0
-	blur(frame, frame_out, Size(Gauss_size, Gauss_size));
-	namedWindow("高斯滤波", WINDOW_NORMAL);
-	imshow("高斯滤波", frame_out);
-	waitKey(30);
-}
-void Gauss_fil_sizeChange(int, void*)
-{
-	Gauss_proc();
-}
-void test5_3()
-{
-	cap.open(0);
-	while (!cap.isOpened()); {
-		cap.open(0);
-		std::cout << "不能打开视频文件" << std::endl;
-	}
-	Gauss_proc();
-	createTrackbar("高斯尺寸", "高斯滤波", &Gauss_size, 10, Gauss_fil_sizeChange);
-	while (1)
-	{
-		Gauss_proc();
-		createTrackbar("高斯尺寸", "高斯滤波", &Gauss_size, 10, Gauss_fil_sizeChange);
-	}
-}
-//--------------------------------------------------------------------------------------------//
-//练习4 边缘提取 读取摄像头图像，并对摄像头图像进行边缘提取分别提取x，y方向上的边缘，观察结果有何区别。
-int Sobel_size = 3;
-cv::Mat frame_outx, frame_outy;
-cv::Mat abs_grad_outx, abs_grad_outy;
-void Sobel_fil_sizeChange(int, void*);
-void Sobel_proc() {
-	bool rSucess = cap.read(frame);
-	if (!rSucess) {
-		std::cout << "不能从视频文件读取帧" << std::endl;
-	}
-	namedWindow("边缘原图", WINDOW_NORMAL);
-	imshow("边缘原图", frame);
-	cvtColor(frame, frame, CV_BGR2GRAY);
-	//threshold(frame, frame, 100, 255, THRESH_BINARY);
-	Sobel(frame, frame_outx, CV_16S, 1, 0, Sobel_size * 2 + 1);//输出图像的深度、x差分阶数、y差分阶数，sobel核大小：奇数
-	//convertScaleAbs(frame_outx, abs_grad_outx);
-	namedWindow("x边缘提取", WINDOW_NORMAL);
-	imshow("x边缘提取", frame_outx);
-	Sobel(frame, frame_outy, CV_16S, 0, 1, Sobel_size * 2 + 1);//输出图像的深度、x差分阶数、y差分阶数，sobel核大小：奇数
-	//convertScaleAbs(frame_outy, abs_grad_outy);
-	namedWindow("y边缘提取", WINDOW_NORMAL);
-	imshow("y边缘提取", frame_outy);
-
-	waitKey(30);
-}
-void Sobel_fil_sizeChange(int, void*)
-{
-	Sobel_proc();
-}
-void test5_4()
-{
-	cap.open(0);
-	while (!cap.isOpened()); {
-		cap.open(0);
-		std::cout << "不能打开视频文件" << std::endl;
-	}
-	Sobel_proc();
-	createTrackbar("边缘内核", "边缘原图", &Sobel_size, 10, Sobel_fil_sizeChange);
-	while (1)
-	{
-		Sobel_proc();
-		createTrackbar("边缘内核", "边缘原图", &Sobel_size, 10, Sobel_fil_sizeChange);
-	}
-}
 int main()
 {
-	test12_1();
+	test13_1();
 	return 0;
 }
